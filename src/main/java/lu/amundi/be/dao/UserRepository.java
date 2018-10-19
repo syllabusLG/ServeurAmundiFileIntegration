a9ab9a1d60550f5df7d6350e3b5db1d3678d1580
Abousyllaba Ndiaye <abousyllaba.ndiaye@devoteam.lu>
2018-10-19 10:25:31 +0200

    correction in user repositories

--- src/main/java/lu/amundi/be/dao/UserRepository.java
+++ src/main/java/lu/amundi/be/dao/UserRepository.java
@@ -1,11 +1,17 @@
 package lu.amundi.be.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import lu.amundi.be.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	public User findByUsername(String username);
+	@Query("select u from User u where u.username like :x")
+	public Page<User> chercher(@Param("x")String mc, Pageable pageable);
 
 }
